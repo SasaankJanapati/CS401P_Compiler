723792bd72fdbdfd22ef34321b0766d34cc40865
sonith17 <muppirivishnusonith@gmail.com>
2025-11-04 00:33:03 +0530

    lib update

--- lib_out/asm/terminos.asm
+++ lib_out/asm/terminos.asm
@@ -62,7 +62,84 @@ PUSH 0
 LOAD_ARG 0 ; Load 'this' to access member object 'Eorigtermios'
 GETFIELD 0
 INVOKEVIRTUAL 2 ; Call TerminalHandler.tcgetattr@I@Termios
-PUSH STR_0  ; Push address of string literal "Raw mode enabled.\n"
+PUSH 19 ; String literal length
+NEWARRAY C ; Create char array for string "Raw mode enabled.\n"
+DUP ; Duplicate array ref for ASTORE
+PUSH 0 ; Push index 0
+PUSH 82 ; Push char 'R'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 1 ; Push index 1
+PUSH 97 ; Push char 'a'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 2 ; Push index 2
+PUSH 119 ; Push char 'w'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 3 ; Push index 3
+PUSH 32 ; Push char ' '
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 4 ; Push index 4
+PUSH 109 ; Push char 'm'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 5 ; Push index 5
+PUSH 111 ; Push char 'o'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 6 ; Push index 6
+PUSH 100 ; Push char 'd'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 7 ; Push index 7
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 8 ; Push index 8
+PUSH 32 ; Push char ' '
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 9 ; Push index 9
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 10 ; Push index 10
+PUSH 110 ; Push char 'n'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 11 ; Push index 11
+PUSH 97 ; Push char 'a'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 12 ; Push index 12
+PUSH 98 ; Push char 'b'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 13 ; Push index 13
+PUSH 108 ; Push char 'l'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 14 ; Push index 14
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 15 ; Push index 15
+PUSH 100 ; Push char 'd'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 16 ; Push index 16
+PUSH 46 ; Push char '.'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 17 ; Push index 17
+PUSH 92 ; Push char '\'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 18 ; Push index 18
+PUSH 110 ; Push char 'n'
+ASTORE ; Store char in array
 PUSH 18
 PUSH 1
 SYS_CALL WRITE ; write
@@ -99,7 +176,108 @@ RET
 .method TerminalHandler.tcsetattr@I@I@Termios
 .limit stack 4
 .limit locals 4
-PUSH STR_1  ; Push address of string literal "Termios attributes set.\n"
+PUSH 25 ; String literal length
+NEWARRAY C ; Create char array for string "Termios attributes set.\n"
+DUP ; Duplicate array ref for ASTORE
+PUSH 0 ; Push index 0
+PUSH 84 ; Push char 'T'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 1 ; Push index 1
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 2 ; Push index 2
+PUSH 114 ; Push char 'r'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 3 ; Push index 3
+PUSH 109 ; Push char 'm'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 4 ; Push index 4
+PUSH 105 ; Push char 'i'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 5 ; Push index 5
+PUSH 111 ; Push char 'o'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 6 ; Push index 6
+PUSH 115 ; Push char 's'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 7 ; Push index 7
+PUSH 32 ; Push char ' '
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 8 ; Push index 8
+PUSH 97 ; Push char 'a'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 9 ; Push index 9
+PUSH 116 ; Push char 't'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 10 ; Push index 10
+PUSH 116 ; Push char 't'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 11 ; Push index 11
+PUSH 114 ; Push char 'r'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 12 ; Push index 12
+PUSH 105 ; Push char 'i'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 13 ; Push index 13
+PUSH 98 ; Push char 'b'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 14 ; Push index 14
+PUSH 117 ; Push char 'u'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 15 ; Push index 15
+PUSH 116 ; Push char 't'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 16 ; Push index 16
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 17 ; Push index 17
+PUSH 115 ; Push char 's'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 18 ; Push index 18
+PUSH 32 ; Push char ' '
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 19 ; Push index 19
+PUSH 115 ; Push char 's'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 20 ; Push index 20
+PUSH 101 ; Push char 'e'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 21 ; Push index 21
+PUSH 116 ; Push char 't'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 22 ; Push index 22
+PUSH 46 ; Push char '.'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 23 ; Push index 23
+PUSH 92 ; Push char '\'
+ASTORE ; Store char in array
+DUP ; Duplicate array ref for ASTORE
+PUSH 24 ; Push index 24
+PUSH 110 ; Push char 'n'
+ASTORE ; Store char in array
 PUSH 25
 PUSH 1
 SYS_CALL WRITE ; write
@@ -117,7 +295,3 @@ INVOKEVIRTUAL 0 ; Call default ctor for Termios
 PUTFIELD 0 ; Store new instance to 'Eorigtermios'
 RET
 .endmethod
-
-.data
-STR_0: .word "Raw mode enabled.\n"
-STR_1: .word "Termios attributes set.\n"
